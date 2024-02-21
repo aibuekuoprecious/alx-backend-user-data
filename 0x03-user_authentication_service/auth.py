28b79a76a0a0395c3fac836c0c6ec85b7e632aed
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-02-21 10:12:49 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,7 +8,7 @@ from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
 
-from typing import Union
+from typing import Optional
 
 
 def _hash_password(password: str) -> bytes:
@@ -34,7 +34,7 @@ class Auth:
         """
         self._db = DB()
 
-    def register_user(self, email: str, password: str) -> Union[None, User]:
+    def register_user(self, email: str, password: str) -> Optional[User]:
         """Registers a new user with the given email and password."""
         try:
             self._db.find_user_by(email=email)
