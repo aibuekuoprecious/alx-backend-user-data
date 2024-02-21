212072280700116320ecb073298f66561284b428
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-02-21 10:57:43 +0100

    task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -42,8 +42,8 @@ def login() -> str:
 
     user = AUTH.valid_login(email, password)
     if user:
-        session_id = AUTH.create_session(user.id)
-        response = jsonify({"email": user.email, "message": "Logged in"})
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
         response.set_cookie('session_id', session_id)
         return response
     else:
