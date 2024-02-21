9fed7972563221dd8da211b446c3388ddfc0d007
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-02-21 09:00:00 +0100

    auth.py updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,13 +46,13 @@ class Auth:
             return user
 
     def valid_login(self, email: str, password: str) -> bool:
-            """Check if the login credentials are valid.
-            """
-            try:
-                user = self._db.find_user_by(email=email)
-            except NoResultFound:
-                return False
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+        """Check if the login credentials are valid.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
 
     def create_session(self, email: str) -> str:
         """Create a session for the user with the given email
