dbb57e43df43449dcc86ed39a98f0f2bec15e786
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-02-21 08:50:45 +0100

    db.py updated

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,25 +33,15 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database"""
+        """Add a new user to the database
+        """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
-        """Find a user by the given keyword arguments.
-
-        Args:
-            **kwargs: Arbitrary keyword arguments to filter the users.
-
-        Returns:
-            User: The first user found.
-
-        Raises:
-            InvalidRequestError: If no keyword arguments are provided.
-            NoResultFound: If no user is found.
-            MultipleResultsFound: If multiple users are found.
+        """Find a user by the given keyword arguments
         """
         if not kwargs:
             raise InvalidRequestError("No keyword arguments provided.")
@@ -65,17 +55,7 @@ class DB:
             raise NoResultFound("Multiple users found.")
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Update a user's attributes in the database.
-
-        Args:
-            user_id (int): The ID of the user to update.
-            **kwargs: Arbitrary keyword arguments representing the user attributes to update.
-
-        Raises:
-            ValueError: If an invalid attribute is passed.
-
-        Returns:
-            None
+        """Update a user's attributes in the database
         """
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
