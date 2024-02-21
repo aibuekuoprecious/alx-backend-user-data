a12cc21ccd73e1ee04dfc8fbccc1497cf12f4c1e
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-02-21 11:17:25 +0100

    task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,13 +70,12 @@ class Auth:
         """
         if session_id is None:
             return None
-
         try:
-            user = self._db.find_user_by_session_id(session_id)
+            user = self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
-
-        return user
+        else:
+            return user
 
     def destroy_session(self, user_id: int) -> None:
         """Destroy the session for the user with the given user ID.
